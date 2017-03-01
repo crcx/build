f403501ea39d084d27b362f1027d5b250f433f97
Charles Childers <crc@debian>
2017-03-01 16:29:30 +0000

    remove build.h

--- build.c
+++ build.c
@@ -25,7 +25,15 @@
 #include <time.h>
 #include <ctype.h>
 
-#include "build.h"
+#define GCC_COMPILE "gcc -c %s %s"
+
+void    err(char *s, char *s2);
+void    add(char *dst, char *src);
+time_t  filedate(char *fn);
+int     dep(char *fn, time_t t);
+int     build(char *fn, time_t t);
+void    make(char *fn);
+int     main(int argc, char **argv);
 
 
 /******************************************************
